3f0f046d2c58db2e28b5232c99b75324144773eb
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2021-11-16 14:07:24 -0500

    add background folder

---
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  //creates the window width and height to be the size of my browser
+  createCanvas(windowWidth, windowHeight);
+  //gives RGB red background
+  background(255, 255, 0);
+}
+
+function draw() {
+
+}
