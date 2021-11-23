cfcac814a73b23a3c78dfa1c52e9997b9f31af68
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2021-11-22 19:28:29 -0500

    add 2x2 grid

--- CreativeCoding/P5/BackgroundColor/sketch.js
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   //creates the window width and height to be the size of my browser
   createCanvas(windowWidth, windowHeight);
   //gives RGB red background
-  background(255, 255, 0);
+  background(255, 100, 0);
 }
 
 function draw() {
