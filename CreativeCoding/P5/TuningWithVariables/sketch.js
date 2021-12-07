be4eff2e302babb81d63acd8567ad857ca38c551
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2021-12-07 08:52:22 -0500

    create bg

--- CreativeCoding/P5/TuningWithVariables/sketch.js
+++ CreativeCoding/P5/TuningWithVariables/sketch.js
@@ -1,32 +1,33 @@
-function setup() {//runs once
-  createCanvas(windowWidth, windowWidth);
-  fill("purple");
+let gif;
+
+function preload (){
+  gif = loadImage ('Assets/SorensenBackground.gif');
+}
+
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('indigo');
   strokeWeight(1);
 }
 
-function draw() {//runs in a loop
+function draw() {
+background (255);
 
+translate(-100, -100)
 
-  //variable for number of squares in array
-  var num = 10;
-  //variable for side length of each square
-  var sideLen = windowWidth/num;
+var num = 10; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
 
-  //create rows in y direction
-  for(var y = 0; y < windowWidth; y = y + sideLen){
+  for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+    image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
 
-    //loop creates a row in x direction
-    for(var x = 0; x < windowWidth; x = x+sideLen){
-
-      quad(x,y,
-      x+sideLen,y,
-      x+sideLen,y + sideLen,
-      x,y + sideLen);
-
-    }
 
   }
+}
 
+}
 
-  noLoop(); //runs function once
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
