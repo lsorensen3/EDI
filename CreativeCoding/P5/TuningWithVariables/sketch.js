43fa1ecb871e20a46ed52ec07ad06dd4f0046e71
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2021-12-07 08:56:11 -0500

    slow background

---
+++ CreativeCoding/P5/TuningWithVariables/sketch.js
@@ -0,0 +1,33 @@
+let gif;
+
+function preload (){
+  gif = loadImage ('Assets/SorensenBackground.gif');
+}
+
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('indigo');
+  strokeWeight(1);
+}
+
+function draw() {
+background (255);
+
+translate(-100, -100)
+
+var num = 10; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
+
+  for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < 2*windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+    image(gif, x, y, windowWidth*.9/num, windowWidth*.9/num);
+
+
+  }
+}
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
