b2dff3c931baaa5f8f0d31040ae0c0bbbc841dd5
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2022-01-06 13:11:09 -0500

    add random pattern and export svg folders

---
+++ CreativeCoding/MatchDraw/randomPattern/sketch.js
@@ -0,0 +1,31 @@
+/* export SVG
+DDF 2019
+need to have p5.svg.js in project and in index.html
+see -https://github.com/zenozeng/p5.js-svg
+this will save an SVG file in your download folder
+*/
+
+var pageWidth = 11; //width of paper in inches
+var pageHeight = 8.5; //height of paper in inches
+
+function setup() {
+  createCanvas(pageWidth * 72, pageHeight * 72, SVG); // Create SVG Canvas
+  strokeWeight(1); // do 0.1 for laser
+  stroke(255, 0, 0); // red is good for laser
+  noFill(); // better not to have a fill for laser
+  //background(230);
+}
+
+function draw() {
+  translate(((pageWidth*72)-600)/2,((pageHeight*72)-600)/2);
+  for(var y = 0; y < 600; y+=random(50,100)){
+    for(var x = 0; x < 600; x+=random(50,100)){
+      line(x,y,100+x,100+y);
+
+    }
+  }
+  //uncomment below to be able to download (svg) the image created
+  //save("mySVG.svg"); // give file name
+  //print("saved svg");
+  noLoop(); // we just want to export once
+}
